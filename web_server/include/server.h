d82c6f02dd41d9e8be69190eb9c6ea8bcd460fbd
Benson Ton <bensonton@Bensons-MacBook-Pro.local>
2023-10-15 21:07:51 -0700

    setup server not done

--- web_server/include/server.h
+++ web_server/include/server.h
@@ -1,11 +1,21 @@
 /*
-
 server.h
-
-
 */
 #ifndef Server_H
 #define Server_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdbool.h>
+
+struct Server
+{
+    int domain;
+    int protocol;
+    sa_family_t sa_family;
+
+
+}
 
 int sum_two(int, int);
 
